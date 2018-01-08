048551591c1de52912b5cc9de7bc6465d5fee6fe
pashko.ostap <pashko.ostap@gmail.com>
2018-01-08 13:31:49 +0200

    wrote tests for checking router-outlet directive

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,47 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing'; 
+import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router/src/directives/router_outlet';
+import { RouterLinkWithHref } from '@angular/router/src/directives/router_link';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [RouterTestingModule.withRoutes([])],
+        declarations: [AppComponent]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges(); 
+    fixture.detectChanges();
+  });
+
+  it('should have a router-outlet directive', () => {
+    let debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(debugElement).not.toBeNull();
+  });
+
+  it('should have a link to todos page', () => {
+    let debugElements = fixture.debugElement.queryAll(
+      By.directive(RouterLinkWithHref)
+    );
+
+    let index = debugElements.findIndex(
+      de => de.properties['href'] === '/todos'
+    );
+
+    expect(index).toBeGreaterThan(-1);
   });
 });
